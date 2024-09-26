c268d68a3e4483f861ad3c0eb7c47df08b0cef15
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2024-09-26 09:44:17 +0100

    Add task 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+
+  for (const value of array) {
+    newArray.push(value + appendString);
+  }
+
+  return newArray;
+}
