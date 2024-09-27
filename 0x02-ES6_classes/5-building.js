4d2f853efce4061fbfd78804ab5a37e9c8e728fd
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2024-09-27 19:25:22 +0100

    Add task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,1 +1,24 @@
-#!/usr/bin/node
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+
+    if (this.constructor !== Building
+      && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+
+    this._sqft = value;
+  }
+
+  evacuationWarningMessage() {}
+}
