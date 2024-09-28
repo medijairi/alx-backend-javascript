c2ce775629c76593eb31bac6302b418e62eeef2e
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2024-09-28 01:00:22 +0100

    Add task 7

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,1 +1,34 @@
-#!/usr/bin/node
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+
+    this._code = value;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
