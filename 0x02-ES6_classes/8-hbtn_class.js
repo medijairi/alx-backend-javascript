819e7eb36ec563a52e582c33de28d3a2ac6dbc50
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2024-09-28 01:22:55 +0100

    Add task 8

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,1 +1,42 @@
-#!/usr/bin/node
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Name must be a number');
+    }
+
+    this._size = value;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    this._location = value;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this.size;
+    }
+
+    if (hint === 'string') {
+      return this.location;
+    }
+
+    return null;
+  }
+}
