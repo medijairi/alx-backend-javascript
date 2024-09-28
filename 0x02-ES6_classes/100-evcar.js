83f4ced045e396ed7f59d87df678bc584d25e63e
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2024-09-28 03:12:03 +0100

    Add task 11

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  cloneCar() {
+    return new Car();
+  }
+}
