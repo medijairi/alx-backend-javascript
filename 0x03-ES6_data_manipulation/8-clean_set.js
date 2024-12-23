9dee2dcd8b4401ae5d0e17268607fdc6cd02ffa8
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2024-12-23 15:43:40 +0100

    Fix task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 export default function cleanSet(set, startString) {
-  if (!startString) {
+  if (typeof startString !== 'string' || startString === '') {
     return '';
   }
 
   const len = startString.length;
 
   return Array.from(set)
-    .filter((value) => value.startsWith(startString))
+    .filter((value) => typeof value === 'string' && value.startsWith(startString))
     .map((value) => value.slice(len))
     .join('-');
 }
