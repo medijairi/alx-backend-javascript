46c2056dc8a0e2463d3fe48098afe8cbc96dd11e
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2025-03-14 00:01:32 +0000

    Add tasks

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,23 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (
+  firstName: string,
+  lastName: string
+): string => {
+  return `${firstName[0]}. ${lastName}`;
+};
